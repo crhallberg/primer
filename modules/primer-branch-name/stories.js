bb9b5cad1e6346a2fe56f35c25357b41e093e44b
Shawn Allen <shawn.allen@github.com>
2018-05-04 14:48:41 -0700

    update primer-branch-name to use new markdown-to-stories API

--- modules/primer-branch-name/stories.js
+++ modules/primer-branch-name/stories.js
@@ -1,10 +1,7 @@
 import React from 'react'
-import { storiesOf } from '@storybook/react'
-import storiesFromMarkdown from '../../tools/stories-from-markdown'
+import {storiesOf} from '@storybook/react'
+import {addAllStoriesFromMarkdown} from '../../tools/stories-from-markdown'
 
 const stories = storiesOf('Branch Name', module)
 
-storiesFromMarkdown(require.context('.', true, /\.md$/))
-  .forEach(({title, story}) => {
-    stories.add(title, story)
-  })
+addAllStoriesFromMarkdown(stories, require.context('.', true, /\.md$/))
